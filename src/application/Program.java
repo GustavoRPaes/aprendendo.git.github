9686e0ce0b62671c78ee2b1efd745dc31d934099
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2021-11-23 20:17:03 -0300

    Testando mais um commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Aprendendo e treiando Git e Github!");
-
+		System.out.println("Testando mais um commit");
 	}
 
 }
